10872aea183331f52640705bf3a7875a1666020c
mnaza <andrey@cossacklabs.com>
2015-06-08 22:39:48 +0300

    IOS clien initial message and session creation added

--- ios/WebsocketExampleClient/WebsocketExampleClient/ViewController.h
+++ ios/WebsocketExampleClient/WebsocketExampleClient/ViewController.h
@@ -8,13 +8,23 @@
 
 #import <UIKit/UIKit.h>
 #import <SocketRocket/SRWebSocket.h>
+#import "objcthemis/ssession.h"
 
-@interface ViewController : UIViewController <SRWebSocketDelegate, UITextFieldDelegate>
+@interface Transport : TSSessionTransportInterface
+
+@end
+
+@interface ViewController : UIViewController <SRWebSocketDelegate, UITextFieldDelegate>{
+    Transport* transport;
+    TSSession* session;
+}
 
 @property (weak, nonatomic) IBOutlet UITextView *messagesTextView;
 
 @property (weak, nonatomic) IBOutlet UIView *containerView;
 @property (weak, nonatomic) IBOutlet UITextField *messageTextField;
+
+
 
 - (IBAction)sendMessage:(id)sender;
 
--- ios/WebsocketExampleClient/WebsocketExampleClient/ViewController.m
+++ ios/WebsocketExampleClient/WebsocketExampleClient/ViewController.m
@@ -7,6 +7,23 @@
 //
 
 #import "ViewController.h"
+#import "objcthemis/ssession.h"
+#import "objcthemis/skeygen.h"
+
+char server_key[]="\x55\x45\x43\x32\x00\x00\x00\x2d\x75\x58\x33\xd4\x02\x12\xdf\x1f\xe9\xea\x48\x11\xe1\xf9\x71\x8e\x24\x11\xcb\xfd\xc0\xa3\x6e\xd6\xac\x88\xb6\x44\xc2\x9a\x24\x84\xee\x50\x4c\x3e\xa0";
+size_t server_key_length=45;
+
+@implementation Transport
+
+- (NSData *)publicKeyFor:(NSData *)binaryId error:(NSError **)error{
+    NSString *id = [[NSString alloc] initWithData:binaryId encoding: NSUTF8StringEncoding];
+    if([id isEqualToString: @"server"]){
+        NSData* key = [[NSData alloc] initWithBytes:server_key  length:server_key_length];
+    }
+    return NULL;
+}
+
+@end
 
 @implementation ViewController {
   SRWebSocket *webSocket;
@@ -49,7 +66,7 @@
   webSocket.delegate = nil;
   webSocket = nil;
 
-  NSString *urlString = @"ws://localhost:8080";
+  NSString *urlString = @"ws://192.168.43.213:8080";
   SRWebSocket *newWebSocket = [[SRWebSocket alloc] initWithURL:[NSURL URLWithString:urlString]];
   newWebSocket.delegate = self;
 
@@ -60,8 +77,23 @@
 #pragma mark - SRWebSocket delegate
 
 - (void)webSocketDidOpen:(SRWebSocket *)newWebSocket {
-  webSocket = newWebSocket;
-  [webSocket send:[NSString stringWithFormat:@"Hello from %@", [UIDevice currentDevice].name]];
+    webSocket = newWebSocket;
+    TSKeyGen * keygenEC = [[TSKeyGen alloc] initWithAlgorithm:TSKeyGenAsymmetricAlgorithmEC];
+    
+    if (!keygenEC) {
+        NSLog(@"%s Error occured while initializing object keygenEC", sel_getName(_cmd));
+        return;
+    }
+    
+    NSData *privateKey = keygenEC.privateKey;
+    NSLog(@"EC private key: %@", privateKey);
+    
+    NSData *publicKey = keygenEC.publicKey;
+    NSLog(@"EC public key:%@", publicKey);
+
+  [webSocket send:[NSString stringWithFormat:@"%@:%@", [UIDevice currentDevice].name, [publicKey base64EncodedStringWithOptions:0]]];
+    transport = [Transport alloc];
+    session = [[TSSession alloc] initWithUserId:[[UIDevice currentDevice].name dataUsingEncoding:NSUTF8StringEncoding] privateKey:privateKey callbacks:transport];
 }
 
 - (void)webSocket:(SRWebSocket *)webSocket didFailWithError:(NSError *)error {
